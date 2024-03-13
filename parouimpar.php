f2e23e0f281233e5e49ae6a9aa7136fe2500f507
Danilo Felype <danilo87651@gmail.com>
2024-03-13 08:32:55 -0300

    primeiro commit

---
+++ parouimpar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Par ou Impar</title>
+</head>
+<body>
+    
+<h1>Código para verificação - Par ou Impar</h1>
+<!-- Código PHP -->
+<?php
+    $num = 4;
+
+    if($num % 2 == 0){
+        echo "O número informado é PAR";
+    }else{
+        echo "O número informado é IMPAR";
+    }
+?>
+
+</body>
+</html>
